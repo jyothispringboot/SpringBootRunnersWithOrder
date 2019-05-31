f88981c7525a6e4664b902d0315594c4e9a5e4c4
Jyothi <Jyothi@JyothiPC>
2019-06-01 01:26:35 +0530

    VersionFirst

---
+++ src/main/java/com/app/runner/AppARunner.java
@@ -0,0 +1,17 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(3)
+public class AppARunner implements CommandLineRunner {
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("From A Runner");
+
+	}
+
+}
